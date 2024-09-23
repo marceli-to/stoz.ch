698391dc73fbfa844177b312da04f079067d7204
Marcel Stadelmann <m@marceli.to>
2024-09-23 10:25:28 +0200

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'Wir machen Geschichten. Panda & Pinguin ist die Agentur für zeitgemässe Kommunikation.'
+  'meta_description' => 'Die Werbeagentur setzt auf wirkungsvolle, überraschende Kommunikation, auf allen Kanälen der klassischen Werbung und neuen Medien. Wir legen Wert auf den persönlichen Kontakt und einer lösungsorientierten Zusammenarbeit. Mit klaren Strategien, Ideenreichtum und perfekten Umsetzungen setzen wir entscheidende Akzente, eindrücklich, einfach, einzigartig.'
 ];
